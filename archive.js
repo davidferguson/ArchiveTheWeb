f7844742505c33d8d1ac9034dc7fbdaa7fc158b4
David Ferguson <davidferguson@users.noreply.github.com>
2016-04-08 16:33:41 +0100

    updated archive.js
    added detection for page URL changes, along with support for archiving images, scripts, and css which the Wayback Machine sometimes misses

--- archive.js
+++ archive.js
@@ -4,7 +4,55 @@ All we're doing is sending a message to the background script, which will go
 and archive the page for us.
 */
 
-chrome.extension.sendMessage(
+function archivePage()
 {
-	data: window.location.href
-});
+	// Archive the HTML of this page
+	chrome.extension.sendMessage(
+	{
+		data: window.location.href
+	});
+
+	// Archive all the images on this page
+	var images = document.getElementsByTagName("img");
+	for (var i = 0; i < images.length; i++)
+	{
+		chrome.extension.sendMessage(
+		{
+			data: images[i].src
+		});
+	}
+
+	// Archive all the scripts on this page
+	var images = document.getElementsByTagName("script");
+	for (var i = 0; i < images.length; i++)
+	{
+		chrome.extension.sendMessage(
+		{
+			data: images[i].src
+		});
+	}
+
+	// Archive all the css and favicons on this page
+	var images = document.getElementsByTagName("link");
+	for (var i = 0; i < images.length; i++)
+	{
+		chrome.extension.sendMessage(
+		{
+			data: images[i].href
+		});
+	}
+}
+
+
+archivePage();
+
+// Constantly check to see if the URL has changed, and if so, archive the page again
+var oldLocation = location.href;
+setInterval(function()
+{
+	if(location.href != oldLocation)
+	{
+		archivePage();
+		oldLocation = location.href
+	}
+}, 5000);
